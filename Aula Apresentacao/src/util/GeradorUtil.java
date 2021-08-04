3a6a48739affb6455bf2b9307753d4f0980cd358
bruno.senna <bruno.senna@PLE111D25.alunos.sc.senac.br>
2021-08-03 21:59:21 -0300

    Aula 04

--- Aula Apresentacao/src/util/GeradorUtil.java
+++ Aula Apresentacao/src/util/GeradorUtil.java
@@ -32,8 +32,52 @@ public class GeradorUtil {
         return gerarNumero(3)+ "." + gerarNumero(3) + "." + gerarNumero(3) + "-"
                 + gerarNumero(2);
         
+        
     }
 
+    public String gerarCnpj() {
+        return gerarNumero(2) + "." + gerarNumero(3) + "." + gerarNumero(3)
+                + "/" + gerarNumero(3) + "-" + gerarNumero(2);
+    }
+
+ 
+
+    public String gerarTelefoneFixo() {
+        return "(48)3" + gerarNumero(3) + "-" + gerarNumero(4);
+    }
+    
+    public String gerarCep() {
+        return gerarNumero(5) + "-" + gerarNumero(3);
+    }
+
+ 
+
+    public String gerarCelular() {
+        return "(48) 9" + gerarNumero(4) + "-" + gerarNumero(4);
+    }
+
+ 
+
+    public String gerarNome() {
+        String[] nomes = {"Silvio", "João", "Maria", "Pedro", "Lucas", "josé",
+            "Marisa", "Tânia", "Sueli", "Tati", "Antônio", "Francisco", "Giogio",
+            "Samara", "Hugo", "Mariele", "William", "Suelen", "Otávio", "Ana" , 
+            "Claudio , Claudia"};
+
+        int tamanhoLista = nomes.length;
+        int indice = (int) (Math.random() * tamanhoLista);
+        return  nomes[indice] + " " + gerarSobrenome();      
+    }
+    
+    private String gerarSobrenome(){
+        String[] sobrenomes = {"Silva", "Motto", "Mota", "Oliveira", "Abravaneu", "Rocha",
+            "Pereira" , "Pires", "Percicotte", "Santos", "Souza", "Zen",
+            "Stark", "Peixes","Fischer", "Smitch", "Tango", "Carvalho",
+            "Peixoto", "Alves", "Almeida" ,"Parker"};
+        int indice = (int) (Math.random() * sobrenomes.length);
+        return sobrenomes[indice];
+    }
+  
     public double gerarSalario() {
         int numero = (int) (Math.random() * 100000);
         return numero;
@@ -43,6 +87,9 @@ public class GeradorUtil {
         GeradorUtil util = new GeradorUtil();
         String cpf = util.gerarCpf();
         System.out.println("CPF: " + cpf);
+        String nome = util.gerarNome();
+        
+        System.out.println("sobrenome: " + nome);
     }
 
 }
